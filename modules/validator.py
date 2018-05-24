fb436fdec4ded8d2bc27562aebcd4260d0bb197c
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-05-24 17:47:02 +0200

    Refactor validator

--- modules/validator.py
+++ modules/validator.py
@@ -2,45 +2,41 @@
 import sys, os, json
 from modules.formatter import format_rules
 
-def validate_fields(fields, product_type_id):
-    if not "ARTNR" in fields:
-        return "KEINE_ARTNR"
-    if not "DELSTAT" in fields:
-        return "KEIN_DELSTAT"
-    if not "TECHDATA" in fields:
-        return "KEIN_TECHDATA"
-    if not fields["TECHDATA"]:
-        return "TECHDATA_LEER"
-    if not product_type_id in fields["TECHDATA"]:
-        return "KEIN_PRODUKTTYP"
-    return None
+# Hilfsmethoden
 
-def validate_required_fields(config, required_fields):
+def validate_required_fields(config, file_path, required_fields, prefix = ""):
     for required_field in required_fields:
         if not required_field in config:
             sys.exit(
-                "[FEHLER] {} enthält kein Feld '{}'"
-                .format(file_path, required_field)
+                "[FEHLER] {}{} enthält kein Feld '{}'"
+                .format(prefix, file_path, required_field)
             )
+
+def validate_list(config, field, file_path, insertion = ""):
+    if not isinstance(config[field], list):
+        sys.exit(
+            "[FEHLER] Das Feld '{}' {}in {} enthält keine Liste"
+            .format(field, insertion, file_path)
+        )
+
+# Teile von der Setup-Validierung
 
 def validate_combinations(config, export_config_path):
     if "kombinationen" in config:
         for name, combination in config["kombinationen"].items():
-            if not "separator" in combination:
-                sys.exit(
-                    "[FEHLER] Kombination {} in {} enthält kein Feld 'separator'"
-                    .format(name, export_config_path)
-                )
-            if not "felder" in combination:
-                sys.exit(
-                    "[FEHLER] Kombination {} in {} enthält kein Feld 'felder'"
-                    .format(name, export_config_path)
-                )
-            if not isinstance(combination["felder"], list):
-                sys.exit(
-                    "[FEHLER] Das Feld 'felder' in Kombination {} in {} enthält keine Liste"
-                    .format(name, export_config_path)
-                )
+            required_fields = ["separator", "felder"]
+            validate_required_fields(
+                combination,
+                export_config_path,
+                required_fields,
+                "Kombination {} in ".format(name)
+            )
+            validate_list(
+                combination,
+                "felder",
+                export_config_path,
+                "in Kombination {} ".format(name)
+            )
 
 def validate_formatters(config, export_config_path):
     if "formatierungen" in config:
@@ -51,46 +47,31 @@ def validate_formatters(config, export_config_path):
                     "[FEHLER] Ungültige Formatierungsregel {} in {}"
                     .format(format_rule, export_config_path)
                 )
-            if not isinstance(config["formatierungen"][format_rule], list):
-                sys.exit(
-                    "[FEHLER] Die Formatierungsregel {} in {} enthält keine Liste"
-                    .format(format_rule, export_config_path)
-                )
-        if "ersetzungen" in config["formatierungen"]:
-            if not isinstance(config["formatierungen"]["ersetzungen"], list):
-                sys.exit(
-                    "[FEHLER] Ersetzungen in {} sind keine Liste"
-                    .format(export_config_path)
-                )
-            for index, replacement in enumerate(config["formatierungen"]["ersetzungen"]):
-                if not "vorher" in replacement:
-                    sys.exit(
-                        "[FEHLER] Die {}. Ersetzung in {} enthält kein Feld 'vorher'"
-                        .format(index + 1, export_config_path)
-                    )
-                if not "nachher" in replacement:
-                    sys.exit(
-                        "[FEHLER] Die {}. Ersetzung in {} enthält kein Feld 'nachher'"
-                        .format(index + 1, export_config_path)
-                    )
-                if not "felder" in replacement:
-                    sys.exit(
-                        "[FEHLER] Die {}. Ersetzung in {} enthält kein Feld 'felder'"
-                        .format(index + 1, export_config_path)
-                    )
-                if not isinstance(replacement["felder"], list):
-                    sys.exit(
-                        "[FEHLER] Das Feld 'felder' in der {}. Ersetzung in {} ist keine Liste"
-                        .format(index + 1, export_config_path)
-                    )
+            validate_list(
+                config["formatierungen"],
+                format_rule,
+                export_config_path,
+                "in 'formatierungen' "
+            )
 
-def validate_export_config(config, export_config_path):
-    required_fields = ["produkttyp" ,"felder"]
-    validate_required_fields(config, required_fields)
-    if not "ARTNR" in config["felder"]:
-        sys.exit("[FEHLER] {} enthält keine ARTNR".format(export_config_path))
-    validate_combinations(config, export_config_path)
-    validate_formatters(config, export_config_path)
+        if "ersetzungen" in config["formatierungen"]:
+            for index, replacement in enumerate(config["formatierungen"]["ersetzungen"]):
+                required_fields = ["vorher", "nachher", "felder"]
+                validate_required_fields(
+                    replacement,
+                    export_config_path,
+                    required_fields,
+                    "Die {}. Ersetzung in 'formatierungen' ".format(index + 1)
+                )
+
+                validate_list(
+                    replacement,
+                    "felder",
+                    export_config_path,
+                    "in der {}. Ersetzung 'formatierungen' ".format(index + 1)
+                )
+
+# Exportierungen
 
 def validate_setup(general_config_file):
     if not os.path.exists(general_config_file):
@@ -102,6 +83,11 @@ def validate_setup(general_config_file):
     config_file = open(general_config_file, "r", encoding="utf-8")
     config = json.load(config_file)
     export_configs_directory = config["configs_directory"]
+    if not os.path.exists(export_configs_directory):
+        sys.exit(
+            "[FEHLER] Die Export-Konfigurationen fehlen, es gibt keinen Ordner {}"
+            .format(export_configs_directory)
+        )
 
     required_fields = [
         "separator",
@@ -110,20 +96,13 @@ def validate_setup(general_config_file):
         "output_directory",
         "archive_directory"
     ]
+    validate_required_fields(config, export_configs_directory, required_fields)
+
     directory_fields = [
         "bsvp_data_directory",
         "output_directory",
         "archive_directory"
     ]
-
-
-    if not os.path.exists(export_configs_directory):
-        sys.exit(
-            "[FEHLER] Die Export-Konfigurationen fehlen, es gibt keinen Ordner {}"
-            .format(export_configs_directory)
-        )
-
-    validate_required_fields(config, required_fields)
     for directory_field in directory_fields:
         if not config[directory_field].endswith("/"):
             sys.exit(
@@ -148,6 +127,22 @@ def validate_setup(general_config_file):
         export_config_path = export_configs_directory + export_config
         with open(export_config_path, "r", encoding="utf-8") as config_file:
             config = json.load(config_file)
-            validate_export_config(config, export_config_path)
+            required_fields = ["produkttyp" ,"felder"]
+            validate_required_fields(config, export_config_path, required_fields)
+            validate_combinations(config, export_config_path)
+            validate_formatters(config, export_config_path)
 
     config_file.close()
+
+def validate_fields(fields, product_type_id):
+    if not "ARTNR" in fields:
+        return "KEINE_ARTNR"
+    if not "DELSTAT" in fields:
+        return "KEIN_DELSTAT"
+    if not "TECHDATA" in fields:
+        return "KEIN_TECHDATA"
+    if not fields["TECHDATA"]:
+        return "TECHDATA_LEER"
+    if not product_type_id in fields["TECHDATA"]:
+        return "KEIN_PRODUKTTYP"
+    return None
