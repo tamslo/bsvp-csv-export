ceda494b8d1218361430718c46f2f49f13841601
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-06-16 09:54:28 +0200

    Validate value type of maximal iteration value in shop config

--- modules/validator/shop_config.py
+++ modules/validator/shop_config.py
@@ -7,6 +7,17 @@ shop_config_value_types = [ "iterierbar", "wert", "prod", "ilugg" ]
 iterable_max_value_types = shop_config_value_types[1:]
 shop_iterable_value_fields = [ "praefix", "max" ]
 
+def value_type(specification):
+    return list(specification.keys())[0]
+
+def validate_value_type(specification, valid_types, name, shop_config_path, insertion=""):
+    value_type_name = value_type(specification)
+    if not value_type_name in valid_types:
+        sys.exit(
+            "[FEHLER] Unbekannter Typ '{}' für Wert{} von '{}' in {}"
+            .format(value_type_name, insertion, name, shop_config_path)
+        )
+
 def validate_shop_config(export_configs_directory, shop_name):
     shop_config_path = export_configs_directory + shop_name + ".json"
     if not os.path.exists(shop_config_path):
@@ -24,16 +35,18 @@ def validate_shop_config(export_configs_directory, shop_name):
                 "[FEHLER] Zu viele Werte in {} für '{}'"
                 .format(shop_config_path, name)
             )
-        key = list(specification.keys())[0]
-        if not key in shop_config_value_types:
-            sys.exit(
-                "[FEHLER] Unbekannter Bezeichner '{}' für Wert von '{}' in {}"
-                .format(key, name, shop_config_path)
-            )
-        if key == "iterierbar":
+        validate_value_type(specification, shop_config_value_types, name, shop_config_path)
+        if value_type(specification) == "iterierbar":
             validate_required_fields(
                 specification["iterierbar"],
                 shop_config_path,
                 shop_iterable_value_fields,
                 "Der iterierbare Wert '{}' in ".format(name)
+            )
+            validate_value_type(
+                specification["iterierbar"]["max"],
+                iterable_max_value_types,
+                name,
+                shop_config_path,
+                " für maximale Iterationen"
             )
