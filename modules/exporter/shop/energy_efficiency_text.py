1cda021537b0c17937668e96dafd1cf711eb30d3
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-06-27 15:45:03 +0200

    Add content to energy_efficiency_text

--- modules/exporter/shop/energy_efficiency_text.py
+++ modules/exporter/shop/energy_efficiency_text.py
@@ -1,6 +1,72 @@
-from .table import header_row
+from . import table
+
+body_rows = [
+    {
+        "description": "Anschlusswert in Watt",
+        "field_id": "0000015"
+    }, {
+        "description": "Energieverbrauch in kWh / 24h",
+        "field_id": "0000089"
+    },
+    {
+        "description": "Energieverbrauch 365 Tage",
+        "field_id": "0000264"
+    },
+    {
+        "description": "Energieeffizienzklasse",
+        "field_id": "0000265"
+    },
+    {
+        "description": "Energieeffizienzindex",
+        "field_id": "0000339"
+    },
+    {
+        "description": "Nettorauminhalt Kühlung gesamt",
+        "field_id": "0000337"
+    },
+    {
+        "description": "Nettorauminhalt Tiefkühlung gesamt",
+        "field_id": "0000338"
+    },
+    {
+        "description": "Kältemittelsorte / GWP",
+        "field_id": "0000139"
+    },
+    {
+        "description": "Klimaklasse",
+        "field_id": "0000142"
+    },
+    {
+        "description": "Geräuschpegel in dB(A)",
+        "field_id": "0000109"
+    },
+    {
+        "description": "Frequenz in Hz",
+        "field_id": "0000099"
+    },
+    {
+        "description": "Spannung in Volt",
+        "field_id": "0000213"
+    }
+]
+
+def get_value(prod_fields, field_id):
+    if field_id in prod_fields:
+        return prod_fields[field_id]
+    else:
+        return ""
 
 def export_energy_efficiency_text(prod_fields):
-    rows = ""
-    rows += header_row("Anschluss- und Verbrauchswerte")
-    return None
+    rows = []
+    rows.append(table.header_row("Anschluss- und Verbrauchswerte"))
+    for row_specification in body_rows:
+        field_id = row_specification["field_id"]
+        rows += table.body_row(
+            row_specification["description"],
+            get_value(prod_fields, field_id)
+        )
+
+    if len(rows) > 1:
+        return table.table("".join(rows))
+    else:
+        return None
--- modules/exporter/shop/table.py
+++ modules/exporter/shop/table.py
@@ -16,8 +16,36 @@ def table(rows):
 def row(cells):
     return "<tr>{}</tr>".format("".join(cells))
 
-def cell(class_string, text):
-    return '<td class="{}">{}</td>'.format(class_string, html.escape(text))
+def html_escape(text):
+    text = html.escape(text)
+    text = text.replace("ä", "&auml;")
+    text = text.replace("ö", "&ouml;")
+    text = text.replace("ü", "&uuml;")
+    text = text.replace("ß", "&szlig;")
+    text = text.replace("Ä", "&Auml;")
+    text = text.replace("Ö", "&Ouml;")
+    text = text.replace("Ü", "&Uuml;")
+    return text
+
+def cell(class_string, text, trailing_space=False):
+    appendix = "&nbsp;" if trailing_space else ""
+    return '<td class="{}">{}{}</td>'.format(
+        class_string,
+        html_escape(text),
+        appendix
+    )
+
+def description_cell(text):
+    return cell("kb-Tleft", text, trailing_space=True)
+
+def value_cell(text):
+    return cell("kb-Tright", text)
+
+def body_row(description, value):
+    return row([
+        description_cell(description),
+        value_cell(value)
+    ])
 
 def header_row(title):
     class_string = "kb-THeaderLeft"
