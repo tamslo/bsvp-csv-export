5fe8cf2d14cf97b92d3611b13d5a7cf5b334fce1
Tamara Slosarek <tamaraslosarek@gmail.com>
2018-07-03 15:55:53 +0200

    Fix tooltip

--- modules/exporter/shop/table.py
+++ modules/exporter/shop/table.py
@@ -8,16 +8,26 @@ class Table():
         self.content_rows = 0
 
     def __include_tooltip(self, text):
+        # Erstetze Begriffe im Text die durch Tooltips ergänzt werden sollen
+        # erst durch Platzhalter, damit Begriffe in Tooltips nicht ersetzt
+        # werden
+        tooltip_placeholders = {}
+
         for tooltip_key, tooltip_value in self.tooltips.items():
             tooltip_key = html_escape(tooltip_key)
             if re.search(r'\b' + re.escape(tooltip_key) + r'\b', text):
-                tooltip_value = html_escape(tooltip_value)
                 tooltip = '<span class="kb-tooltip" title="'
-                tooltip += tooltip_value
+                tooltip += html_escape(tooltip_value)
                 tooltip += '">'
                 tooltip += tooltip_key
                 tooltip += "</span>"
-                text = text.replace(tooltip_key, tooltip)
+
+                tooltip_placeholder = "$${}$$".format(tooltip_key)
+                tooltip_placeholders[tooltip_placeholder] = tooltip
+                text = text.replace(tooltip_key, tooltip_placeholder)
+
+        for placeholder, tooltip in tooltip_placeholders.items():
+            text = text.replace(placeholder, tooltip)
 
         return text
 
