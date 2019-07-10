3e7471972c9785c39b2ed4888e84b884dae80bb3
Tamara Slosarek <tamaraslosarek@gmail.com>
2019-07-10 13:07:53 +0000

    Adapt log text for shop export warning

--- modules/exporter/shop/utils/get_techdata_value.py
+++ modules/exporter/shop/utils/get_techdata_value.py
@@ -6,11 +6,9 @@ def get_value(fields, field_id, warn=False, prod_fields=None):
         return fields[field_id]
     else:
         if warn:
-            warning_text = "[ACHTUNG] Kein Wert für das Feld '"
+            warning_text = prod_fields["ARTNR"]
+            warning_text += ": Kein Wert für das Feld '"
             warning_text += field_id
-            warning_text += "' im Produkt mit der Artikelnummer "
-            warning_text += prod_fields["ARTNR"]
-            warning_text += " von " + prod_fields["MANUFACTURER"]
             warning_text += ". Das Feld in der Tabelle bleibt leer."
             Logger().log(warning_text)
         return ""
