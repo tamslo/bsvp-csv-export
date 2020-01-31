7573df4d0dc2d2993ce1ec6b0ba49c973c6b6523
Tamara Slosarek <tamaraslosarek@gmail.com>
2020-01-31 15:03:24 +0000

    Add test to custom export

--- modules/exporter/custom.py
+++ modules/exporter/custom.py
@@ -24,8 +24,20 @@ class CustomExporter(BaseExporter):
         csv_path = self.output_directory() + "complete.csv"
         header_fields = list(self.export_config.keys())
         self.maybe_create_csv(csv_path, header_fields)
-        csv_row = list(map(
-            lambda field: prod_fields[field] if field in prod_fields else None,
-            list(self.export_config.values())
-        ))
-        return self.write_csv_row(csv_path, csv_row)
+        include_product = True
+        included_fields = []
+        for field_name, field_value in self.export_config.items():
+            if isinstance(field_value, str):
+                included_fields.append(field_value)
+                continue
+            else:
+                field = field_value["field"]
+                included_fields.append(field)
+                value = prod_fields[field] if field in prod_fields else None
+                include_product = include_product and value != None and field_value["contains"] in value
+        if include_product:
+            csv_row = list(map(
+                lambda field: prod_fields[field] if field in prod_fields else None,
+                included_fields
+            ))
+            return self.write_csv_row(csv_path, csv_row)
