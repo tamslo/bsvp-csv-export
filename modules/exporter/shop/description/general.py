4db1abb447209c9ecbc244583ea94114d20cd8c8
Tamara Slosarek <tamaraslosarek@gmail.com>
2020-08-02 13:14:11 +0000

    Select welcome text according to ARTWELCOMESTATE

--- modules/exporter/shop/description/general.py
+++ modules/exporter/shop/description/general.py
@@ -1,35 +1,9 @@
-from modules.logger import Logger
 from ..utils.include_tooltips import include_tooltips
-
-max_placeholders = 5
-def placeholder(index):
-    return "$BT_Passage{}$".format(index)
-
-def replacement(ilugg_fields, index):
-    return ilugg_fields["P{}Value".format(index)]
+from .welcome_text import get_welcome_text
 
 def export_general_description(parameters):
     prod_fields = parameters["prod_fields"]
     ilugg_fields = parameters["ilugg_fields"]
     tooltips = parameters["tooltips"]
-
-    welcome_text = ""
-    if "WELCOMETEXT" in prod_fields and prod_fields["WELCOMETEXT"] != "":
-        welcome_text = prod_fields["WELCOMETEXT"]
-    elif ("WELCOMETHISTEXT" in prod_fields):
-        welcome_text = prod_fields["WELCOMETHISTEXT"]
-    else:
-        Logger().log("[WARNUNG] {} hat kein WELCOMETEXT und kein WELCOMETHISTEXT Feld".format(
-            prod_fields["ARTNR"]
-        ))
-
-    index = 1
-    while index <= max_placeholders:
-        welcome_text = welcome_text.replace(
-            placeholder(index),
-            replacement(ilugg_fields, index)
-        )
-        index += 1
-
-    text = welcome_text + prod_fields["DESC"]
+    text = get_welcome_text(prod_fields, ilugg_fields) + prod_fields["DESC"]
     return include_tooltips(tooltips, text)
---
+++ modules/exporter/shop/description/welcome_text.py
@@ -0,0 +1,50 @@
+from modules.logger import Logger
+
+def placeholder(index):
+    return "$BT_Passage{}$".format(index)
+
+def replacement(ilugg_fields, index):
+    return ilugg_fields["P{}Value".format(index)]
+
+def get_welcome_text(prod_fields, ilugg_fields):
+    max_placeholders = 5
+    welcome_text = ""
+
+    if not "ARTWELCOMESTATE" in prod_fields:
+        Logger().log("[WARNUNG] {} hat kein ARTWELCOMESTATE Feld".format(
+            prod_fields["ARTNR"]
+        ))
+        return welcome_text
+    welcome_state = prod_fields["ARTWELCOMESTATE"]
+
+    if welcome_state == "0":
+        if not "WELCOMETHISTEXT" in prod_fields:
+            Logger().log("[WARNUNG] {} mit ARTWELCOMESTATE 0 hat kein WELCOMETHISTEXT Feld".format(
+                prod_fields["ARTNR"]
+            ))
+            return welcome_text
+        welcome_text = prod_fields["WELCOMETHISTEXT"]
+
+    if welcome_state == "1":
+        index = 1
+        while index <= max_placeholders:
+            welcome_text = welcome_text + placeholder(ilugg_fields, index)
+            index += 1
+
+    if welcome_state == "2":
+        if not "WELCOMETEXT" in prod_fields:
+            Logger().log("[WARNUNG] {} mit ARTWELCOMESTATE 2 hat kein WELCOMETEXT Feld".format(
+                prod_fields["ARTNR"]
+            ))
+            return welcome_text
+        welcome_text = prod_fields["WELCOMETEXT"]
+
+    index = 1
+    while index <= max_placeholders:
+        welcome_text = welcome_text.replace(
+            placeholder(index),
+            replacement(ilugg_fields, index)
+        )
+        index += 1
+
+    return welcome_text
