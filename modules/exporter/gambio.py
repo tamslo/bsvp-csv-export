ba3be9c35f9b2be4fd3cdd35daa4e9bea76e6403
Tamara Slosarek <tamaraslosarek@gmail.com>
2021-08-22 15:18:57 +0000

    Combine categories in p_cat.0 for Gambio

--- modules/exporter/gambio.py
+++ modules/exporter/gambio.py
@@ -1,7 +1,6 @@
 from .shop import ShopExporter
 from .complete import get_complete_header_fields as get_complete_techdata_fields
 from modules.constants import GAMBIO_NAME, SHOP_NAME, TECHDATA
-from modules.logger import Logger
 
 class GambioExporter(ShopExporter):
     def __init__(self, manufacturers):
@@ -22,9 +21,33 @@ class GambioExporter(ShopExporter):
 
     def extract_information(self, prod_fields, ilugg_fields, attribute_names, attribute_types):
         row = super().extract_information(prod_fields, ilugg_fields, attribute_names, attribute_types)
+        # Fasse Werte von p_cat.x in p_cat.0 zusammen
+        # Lasse die übrigen p_cat Felder leer
+        header_fields = self.header_fields(prod_fields, ilugg_fields)
+        category_prefix = "p_cat."
+        main_category = "{}0".format(category_prefix)
+        main_category_index = None
+        other_category_indices = []
+        category_values = []
+        current_field_index = 0
+        for header_field in header_fields:
+            if header_field.startswith(category_prefix):
+                category_value = row[current_field_index]
+                if category_value != "":
+                    category_values.append(category_value)
+                if header_field == main_category:
+                    main_category_index = current_field_index
+                else:
+                    other_category_indices.append(current_field_index)
+            current_field_index = current_field_index + 1
+        row[main_category_index] = " > ".join(category_values)
+        for other_category_index in other_category_indices:
+            row[other_category_index] = None
+
+        # Füge TECHDATA Felder hinter Shop Feldern an
         for techdata_field in self.techdata_fields:
             value = None
-            if (TECHDATA in prod_fields):
+            if TECHDATA in prod_fields:
                 techdata = prod_fields[TECHDATA]
                 if techdata_field in techdata:
                     value = techdata[techdata_field]
